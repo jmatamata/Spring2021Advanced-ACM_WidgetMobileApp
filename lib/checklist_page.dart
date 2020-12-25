f40328dcd7843297202f9210d82a9cf842658876
Jorge Mata <juniormata@verizon.net>
2020-12-25 00:21:54 -0800

    File and Class name changed.

---
+++ lib/checklist_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChecklistPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Checklist',
+        style: TextStyle(
+            fontWeight: FontWeight.w900, fontSize: 30, fontFamily: 'Roboto'),
+      ),
+    );
+  }
+}
